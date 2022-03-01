b8c9ffec49c607ea4d6484ee353306762a986d5a
Mai <jea989@gmail.com>
2022-03-01 16:04:36 -0500

    updated format to match pies model, fix edge cases

--- get.sh
+++
@@ -1,1 +0,0 @@
-curl https://en.wikipedia.org/wiki/List_of_pies,_tarts_and_flans > wiki.html
--- index.js
+++ index.js
@@ -13,19 +13,51 @@ const parsedjson = tabletojson.convert(html)[0];
 //console.dir(parsedjson, {depth: null})
 //console.dir(htmljson, {depth: null})
 
+const seedjson = [];
+
 for (let i = 0; i < htmljson.length; i++) {
-  const ex = htmljson[i]['Image']
-  if (ex.length > 0) {
-  let tmpThumb = 'https:' + ex.split(' ').filter(word => word.includes('src='))[0].split('"')[1]
-  parsedjson[i].ThumbnailUrl = tmpThumb
+  const imghtml = htmljson[i]['Image']
+  let tmpThumb, tmpType, tmpName
+
+  if (imghtml.length > 0) {
+    tmpThumb = 'https:' + imghtml.split(' ').filter(w => w.includes('src='))[0].split('"')[1]
   } else {
-    parsedjson[i].ThumbnailUrl = ''
+    tmpThumb = '/default.png'
   }
+
+  if (parsedjson[i].Description == 'Sweet or savory') {
+    tmpType = 'Savory or sweet';
+  } else {
+    tmpType = parsedjson[i].Description;
+  }
+  
+
+  if (htmljson[i].Name.includes('<br')) {
+    tmpName = htmljson[i].Name.split('title="')[1].split('"')[0]
+  } else {
+    tmpName = parsedjson[i].Name
+  }
+
+  let tmpPie = {
+    name: tmpName,
+    origin: parsedjson[i].Origin,
+    type: tmpType,
+    description:parsedjson[i].Description,
+    thumbnailurl: tmpThumb,
+    price: Math.floor(Math.random() * (10000 - 100) + 100) / 100, // price between 1.00 and 100.00
+    quantity: Math.floor(Math.random() * (100 - 1) + 1),
+  }
+
+  //console.log(parsedjson[i].Name) 
+  //console.log(htmljson[i].Name) 
+  //console.log(tmpPie) 
 //  console.log(parsedjson[i])
+  //if (i == 1) process.exit();
+  seedjson.push(tmpPie)
 }
   
-const data = JSON.stringify(parsedjson);
-await fs.writeFileSync('pies.json', data);
+const data = JSON.stringify(seedjson);
+await fs.writeFileSync('pies.json', data, {encoding:'utf8', flag:'w'});
 
 }
 
